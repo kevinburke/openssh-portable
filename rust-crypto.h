4c684b9706bb5430df08806e0dece2f564f101d0
Kevin Burke <kevin@burke.dev>
2026-03-24 22:26:30 -0700

    parse: route ssh URI parsing through Rust

--- rust-crypto.h
+++ rust-crypto.h
@@ -15,7 +15,7 @@
 extern "C" {
 #endif
 
-#define OSSH_RUST_CRYPTO_ABI_VERSION 15U
+#define OSSH_RUST_CRYPTO_ABI_VERSION 16U
 #define OSSH_RUST_PARSE_STATUS_OK 0
 #define OSSH_RUST_PARSE_STATUS_INVALID_FORMAT 1
 #define OSSH_RUST_PARSE_STATUS_WRONG_PASSPHRASE 2
@@ -134,6 +134,19 @@ struct ossh_rust_user_host_port_parse {
 	uint32_t has_user;
 	uint32_t has_port;
 };
+struct ossh_rust_uri_parse {
+	size_t user_offset;
+	size_t user_len;
+	size_t host_offset;
+	size_t host_len;
+	size_t port_offset;
+	size_t port_len;
+	size_t path_offset;
+	size_t path_len;
+	uint32_t has_user;
+	uint32_t has_port;
+	uint32_t has_path;
+};
 int ossh_rust_cert_parse_body(const uint8_t *input, size_t input_len,
     struct ossh_rust_cert_body_parse *out);
 size_t ossh_rust_private2_decode_len(const uint8_t *input, size_t input_len);
@@ -158,6 +171,8 @@ int ossh_rust_hpdelim2_parse(uint8_t *input, size_t input_len,
     struct ossh_rust_hpdelim_parse *out);
 int ossh_rust_parse_user_host_port(const uint8_t *input, size_t input_len,
     struct ossh_rust_user_host_port_parse *out);
+int ossh_rust_parse_uri(const uint8_t *input, size_t input_len,
+    struct ossh_rust_uri_parse *out);
 void *ossh_rust_dh_group_new(int group_id);
 int ossh_rust_dh_generate_key(void *group, size_t need_bits);
 size_t ossh_rust_dh_public_len(const void *group);
