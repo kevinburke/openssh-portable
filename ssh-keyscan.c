7acd231968ba086705f84f2641536bd19c3d8528
Kevin Burke <kevin@burke.dev>
2026-03-27 11:08:40 -0700

    kex: register Rust DH handlers in client and server setup

--- ssh-keyscan.c
+++ ssh-keyscan.c
@@ -291,6 +291,14 @@ keygrab_ssh2(con *c)
 	c->c_ssh->kex->kex[KEX_DH_GEX_SHA1] = kexgex_client;
 	c->c_ssh->kex->kex[KEX_DH_GEX_SHA256] = kexgex_client;
 #endif
+#ifdef WITH_RUST_CRYPTO
+	c->c_ssh->kex->kex[KEX_DH_GRP14_SHA1] = kex_gen_client;
+	c->c_ssh->kex->kex[KEX_DH_GRP14_SHA256] = kex_gen_client;
+	c->c_ssh->kex->kex[KEX_DH_GRP16_SHA512] = kex_gen_client;
+	c->c_ssh->kex->kex[KEX_DH_GRP18_SHA512] = kex_gen_client;
+	c->c_ssh->kex->kex[KEX_DH_GEX_SHA1] = kexgex_client;
+	c->c_ssh->kex->kex[KEX_DH_GEX_SHA256] = kexgex_client;
+#endif
 #if defined(WITH_OPENSSL) && defined(OPENSSL_HAS_ECC) || defined(WITH_RUST_CRYPTO)
 	c->c_ssh->kex->kex[KEX_ECDH_SHA2] = kex_gen_client;
 #endif
