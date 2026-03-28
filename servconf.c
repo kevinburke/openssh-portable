5595db3ff9248896d720cb30dcb1efbc7b195081
Kevin Burke <kevin@burke.dev>
2026-03-28 07:38:34 -0700

    parse: route multistate config formatting through Rust

--- servconf.c
+++ servconf.c
@@ -4003,13 +4003,7 @@ parse_server_config(ServerOptions *options, const char *filename,
 static const char *
 fmt_multistate_int(int val, const struct multistate *m)
 {
-	u_int i;
-
-	for (i = 0; m[i].key != NULL; i++) {
-		if (m[i].value == val)
-			return m[i].key;
-	}
-	return "UNKNOWN";
+	return multistate_name(val, m);
 }
 
 static const char *
