4caa16708f7252bd613f58caf09201c752267e27
Kevin Burke <kevin@burke.dev>
2026-03-29 23:47:42 -0700

    parse: route LogFacility output through Rust

--- readconf.c
+++ readconf.c
@@ -4400,7 +4400,7 @@ dump_client_config(Options *o, const char *host)
 #endif
 
 	/* oLogFacility */
-	printf("syslogfacility %s\n", log_facility_name(o->log_facility));
+	dump_cfg_string(oLogFacility, log_facility_name(o->log_facility));
 
 	/* oProxyCommand / oProxyJump */
 	if (o->jump_host == NULL)
