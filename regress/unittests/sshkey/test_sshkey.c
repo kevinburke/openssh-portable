63326f4bfc4136b7c6e52b52ee752e956755dc15
Kevin Burke <kevin@burke.dev>
2026-03-30 22:43:57 -0700

    sshkey: route key type relations through Rust

--- regress/unittests/sshkey/test_sshkey.c
+++ regress/unittests/sshkey/test_sshkey.c
@@ -493,6 +493,13 @@ sshkey_tests(void)
 	TEST_START("metadata lookup helpers");
 	ASSERT_INT_EQ(sshkey_type_from_name("ssh-ed25519"), KEY_ED25519);
 	ASSERT_INT_EQ(sshkey_type_from_shortname("ED25519"), KEY_ED25519);
+	ASSERT_INT_EQ(sshkey_type_plain(KEY_ED25519_CERT), KEY_ED25519);
+	ASSERT_INT_EQ(sshkey_type_is_cert(KEY_ED25519_CERT), 1);
+	k1 = sshkey_new(KEY_ED25519_SK_CERT);
+	ASSERT_PTR_NE(k1, NULL);
+	ASSERT_INT_EQ(sshkey_is_sk(k1), 1);
+	sshkey_free(k1);
+	k1 = NULL;
 #if defined(WITH_OPENSSL) || defined(WITH_RUST_CRYPTO)
 	ASSERT_INT_EQ(sshkey_type_from_name("rsa-sha2-256"), KEY_RSA);
 	ASSERT_INT_EQ(sshkey_type_from_shortname("RSA"), KEY_RSA);
